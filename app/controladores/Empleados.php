7db5e4fb54441b241722dbf9800c87acd4788271
Yesenia <yessy12384@gmail.com>
2021-08-30 22:03:52 -0500

    Gestión de empleados

--- app/controladores/Empleados.php
+++ app/controladores/Empleados.php
@@ -32,4 +32,71 @@ class Empleados extends Controlador
     }
   }
 
+  public function getTipoEmpleado(){
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+      
+      $tipoEmp = $this->modelo->getTipoEmpleado();
+      $resp['status'] = 'OK';
+      $resp['res'] = $tipoEmp;
+      echo json_encode($resp);
+      
+    }else{
+      echo json_encode($this->resp);
+    }
+  }
+
+  public function getEmpleados(){
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+      $empleados = $this->modelo->getEmpleados();
+      $this->resp['status'] = 'OK';
+      $this->resp['res'] = $empleados;
+      echo json_encode($this->resp);
+      
+    }else{
+      echo json_encode($this->resp);
+    }
+  }
+
+  public function nuevoEmpleado(){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $form = json_decode($_POST['form'], true);
+      $valid = $this->modelo->validClave($form['clave']);
+      if($valid){
+        $this->resp['status'] = 'existing';
+      }else{
+        $addE = $this->modelo->nuevoEmpleado($form);
+        if($addE){
+          $this->resp['status'] = 'OK';
+        }else{
+          $this->resp['status'] = 'error';
+          $this->resp['res'] = 'Error de sintaxis';
+        }
+      }
+      echo json_encode($this->resp);
+    }else{
+      echo json_encode($this->resp);
+    }
+  }
+
+  public function editEmpleado(){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $form = json_decode($_POST['form'], true);
+      $valid = $this->modelo->validClave($form['clave']);
+      if($valid){
+        $this->resp['status'] = 'existing';
+      }else{
+        $addE = $this->modelo->editEmpleado($form);
+        if($addE){
+          $this->resp['status'] = 'OK';
+        }else{
+          $this->resp['status'] = 'error';
+          $this->resp['res'] = 'Error de sintaxis';
+        }
+      }
+      echo json_encode($this->resp);
+    }else{
+      echo json_encode($this->resp);
+    }
+  }
+
 }
--- app/modelos/modeloEmpleados.php
+++ app/modelos/modeloEmpleados.php
@@ -43,5 +43,75 @@ class modeloEmpleados
       return false;
     }
   }
+
+  public function getTipoEmpleado()
+  {
+    $tipoEmp = $this->db->select("tipoempleado",
+      [
+        "id_tipoEmpleado",
+        "nombreTipoEmpleado(nombre)"
+      ]
+    );
+    return $tipoEmp;
+  }
+
+  public function getEmpleados()
+  {
+    $empleados = $this->db->select("empleados",
+      [
+        "[><]tipoempleado" => ["id_tipoEmpleado" => "id_tipoEmpleado"]
+      ],
+      [
+        "empleados.id_empleado",
+        "empleados.nombreEmpleado",
+        "empleados.apellidosEmpleado",
+        "empleados.claveAcceso",
+        "tipoempleado.id_tipoEmpleado",
+        "tipoempleado.nombreTipoEmpleado(tipo)"
+      ]
+    );
+    return $empleados;
+  }
+
+  public function nuevoEmpleado($form)
+  {
+    $insert = $this->db->insert("empleados",
+    [
+      "id_tipoEmpleado" => $form['tipo'],
+      "nombreEmpleado" => $form['nombre'],
+      "apellidosEmpleado" => $form['ape'],
+      "claveAcceso" => $form['clave']
+    ]);
+
+    return $insert->rowCount() === 1 ? true : false;
+
+  }
+
+  public function editEmpleado($form)
+  {
+    $update = $this->db->update("empleados",
+      [
+      "id_tipoEmpleado" => $form['tipo'],
+      "nombreEmpleado" => $form['nombre'],
+      "apellidosEmpleado" => $form['ape'],
+      "claveAcceso" => $form['clave']
+      ],
+      [
+        "id_empleado" => $form['id_empleado']
+      ]
+    );
+
+    return $update->rowCount() === 1 ? true : false;
+  }
+
+  public function validClave($clave)
+	{
+    $res = $this->db->has("empleados", [
+    	"AND" => [
+    		"claveAcceso" => $clave
+    	]
+    ]);
+    return $res;
+  }
   
 }
